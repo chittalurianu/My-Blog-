7fbb47bdaea717a4204ec61244b1bb4f7e2a4279
Anu Chittaluri <chittalurianu@gmail.com>
2024-10-30 13:45:42 -0400

    page cgange

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -20,6 +20,7 @@ from Welcome import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'), #this is the URL pattern for the blog home page
 ]
 
